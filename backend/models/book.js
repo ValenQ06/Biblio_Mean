c7f71f7b858416e41b485eddde374d2fa5926ba3
ValenQ06 <vquinonesmina@gmail.com>
2022-02-11 10:45:38 -0500

    Se agregaron las colecciones en la carpeta models

---
+++ backend/models/book.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  user: String,
+  name: String,
+  description: String,
+  author: String,
+  price: Number,
+  imageUrl: String,
+  bookStatus: String,
+  registerDate: { type: Date, default: Date.now },
+});
+
+const book = mongoose.model("books", bookSchema);
+export default book;
